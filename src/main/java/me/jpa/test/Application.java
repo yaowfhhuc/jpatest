5e696ea98c4e350506b913dc35e64e3fea318a1b
yaowf_hhuc <yaowf_hhuc@sina.com>
2016-05-24 17:32:51 +0800

    aaa

---
+++ src/main/java/me/jpa/test/Application.java
@@ -0,0 +1,15 @@
+package me.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		for(String name:context.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
+	}
+}
